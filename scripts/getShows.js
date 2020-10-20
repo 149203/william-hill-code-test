7920d64687829758e4dd6e7f28d663024a86df12
149203 <149203@gmail.com>
2020-10-20 14:48:57 -0700

    add gitignore

--- scripts/getShows.js
+++ scripts/getShows.js
@@ -6,14 +6,15 @@ const showsFile = "../data/shows.json";
 // quick and dirty, let's start shows.json with the following characters: [{}
 
 const startPage = 0;
-const endPage = 9;
+const endPage = 205;
 
 for (i = startPage; i <= endPage; i++) {
    axios
       .get("http://api.tvmaze.com/shows?page=0")
       .then(async function (res) {
-         await res.data.forEach((show) => {
+         await res.data.forEach((show, i) => {
             fs.appendFileSync(showsFile, `, ${JSON.stringify(show)}`);
+            console.log(`${i} shows added.`)
          });
       })
       .catch(function (error) {
